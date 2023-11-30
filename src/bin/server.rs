45bf9d46bd2b46554c9a7cc2222a445a2929984a
b123400 <i@b123400.net>
2023-11-30 12:33:58 +0900

    Actually run the server with config

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,12 @@
-use zika::Server;
-
+use env_logger;
 use zika::config::read_from_default_location;
+use zika::Server;
 
 #[tokio::main]
 async fn main() {
-    let config = read_from_default_location();
-    println!("deserialized = {:?}", config);
+    env_logger::init();
+    let config = read_from_default_location().unwrap();
+    log::debug!("Config = {:?}", config);
+    let mut server = Server::new(config);
+    server.run().await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,29 @@ pub mod config;
 mod lookup_pool;
 mod remote;
 
-pub struct Client {
+pub struct Client {}
 
-}
-
-impl Client {
-
-}
+impl Client {}
 
 pub struct Server {
-
+    remote: remote::Remote,
 }
 
 impl Server {
+    pub fn new(config: config::Config) -> Self {
+        let mqtt_options = config.broker_mqtt_options();
+        match config.server {
+            Some(s) => Self {
+                remote: remote::Remote::new(&mqtt_options, s.topic),
+            },
+            _ => panic!("Server config not found"),
+        }
+    }
 
+    pub async fn run(&mut self) {
+        loop {
+            let x = self.remote.recv().await;
+            log::debug!("recv = {:?}", x);
+        }
+    }
 }
