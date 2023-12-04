1df01a34045804cff72075e05ec8a31894a3df2d
b123400 <i@b123400.net>
2023-12-04 17:06:23 +0900

    Create and read packets from tunnel

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,9 +1,16 @@
-use zika::Client;
-
+use env_logger;
 use zika::config::read_from_default_location;
+use zika::Client;
 
 #[tokio::main]
 async fn main() {
-    let config = read_from_default_location();
-    println!("deserialized = {:?}", config);
+    env_logger::init();
+    let config = read_from_default_location().unwrap();
+    log::debug!("Config = {:?}", config);
+    if let Some(client_config) = config.client {
+        for tun_config in client_config.tunnels.iter() {
+            let mut client = Client::new(tun_config, &config.driver);
+            client.run().await;
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,46 @@ pub mod config;
 mod lookup_pool;
 mod remote;
 
-pub struct Client {}
+use etherparse::Ipv4Header;
+use futures::stream::StreamExt;
+use packet::ip::Packet;
+use std::io::Read;
 
-impl Client {}
+pub struct Client {
+    frame: tokio_util::codec::Framed<tun::AsyncDevice, tun::TunPacketCodec>,
+}
+
+impl Client {
+    pub fn new(
+        client_tunnel_config: &config::ClientTunnel,
+        driver_config: &config::DriverConfig,
+    ) -> Self {
+        let mut config = tun::Configuration::default();
+        config.address(client_tunnel_config.bind_addr.clone());
+        if let Some(tun_config) = &driver_config.tun {
+            config.netmask(tun_config.netmask.clone());
+        }
+        config.up();
+
+        #[cfg(target_os = "linux")]
+        config.platform(|config| {
+            config.packet_information(true);
+        });
+
+        let dev = tun::create_as_async(&config).unwrap();
+        let stream = dev.into_framed();
+        Self { frame: stream }
+    }
+
+    pub async fn run(&mut self) {
+        while let Some(packet) = self.frame.next().await {
+            match packet {
+                Ok(pkt) => log::debug!("pkt: {:#?}", Packet::unchecked(pkt.get_bytes())),
+                Err(err) => panic!("Error: {:?}", err),
+            }
+        }
+    }
+}
 
 pub struct Server {
     remote: remote::Remote,
