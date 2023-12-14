38325cf50138b2088fcd0990132f7bc906adf7fa
b123400 <i@b123400.net>
2023-12-14 21:51:32 +0900

    Better error message

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -5,7 +5,7 @@ use zika::config::read_from_default_location;
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let config = read_from_default_location().unwrap();
+    let config = read_from_default_location().expect("A proper config file");
     log::debug!("Config = {:?}", config);
     let mut client = Client::new(&config).await;
     client.run().await;
