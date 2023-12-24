66ea5c2925f0c26c6488c5de2adf7f94af7294a1
b123400 <i@b123400.net>
2023-12-24 13:45:09 +0900

    Merge base config with broker specific config

--- src/config.rs
+++ src/config.rs
@@ -5,30 +5,20 @@ use serde::Deserialize;
 use std::fs;
 use std::net::Ipv4Addr;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct MqttOptions {
-    #[serde(default = "default_keepalive_interval")]
-    pub keepalive_interval: u64,
+    pub keepalive_interval: Option<u64>,
 
     pub username: Option<String>,
     pub password: Option<String>,
 
     pub ca_file: Option<String>,
 
-    #[serde(default = "default_tls_insecure")]
-    pub tls_insecure: bool,
+    pub tls_insecure: Option<bool>,
     pub key_file: Option<String>,
     pub cert_file: Option<String>,
 
     pub topic_alias_max: Option<u16>,
-}
-
-fn default_keepalive_interval() -> u64 {
-    return 60;
-}
-
-fn default_tls_insecure() -> bool {
-    return false;
 }
 
 #[derive(Deserialize, Debug)]
@@ -91,8 +81,32 @@ pub fn read_from_default_location() -> Result<Config, ConfigError> {
     return Ok(deserialized);
 }
 
+impl MqttOptions {
+    pub fn merge_with_option(&self, another_option: MqttOptions) -> MqttOptions {
+        MqttOptions {
+            keepalive_interval: another_option
+                .keepalive_interval
+                .or(self.keepalive_interval),
+
+            username: another_option.username.or(self.username.clone()),
+            password: another_option.password.or(self.password.clone()),
+
+            ca_file: another_option.ca_file.or(self.ca_file.clone()),
+
+            tls_insecure: another_option.tls_insecure.or(self.tls_insecure),
+            key_file: another_option.key_file.or(self.key_file.clone()),
+            cert_file: another_option.cert_file.or(self.cert_file.clone()),
+
+            topic_alias_max: another_option.topic_alias_max.or(self.topic_alias_max),
+        }
+    }
+}
+
 impl MqttBroker {
-    pub fn to_mqtt_options(&self) -> rumqttc::v5::MqttOptions {
+    pub fn to_mqtt_options(
+        &self,
+        base_mqtt_options: &Option<MqttOptions>,
+    ) -> rumqttc::v5::MqttOptions {
         let mut rng = thread_rng();
         let random_id: String = (&mut rng)
             .sample_iter(Alphanumeric)
@@ -100,9 +114,14 @@ impl MqttBroker {
             .map(char::from)
             .collect();
         let mut options = rumqttc::v5::MqttOptions::new(random_id, &self.host, self.port);
+        let mqtt_options = match (base_mqtt_options, self.options.clone()) {
+            (Some(b), Some(opts)) => Some(b.merge_with_option(opts)),
+            (None, o) => o,
+            (o, None) => o.clone(),
+        };
         // TODO: more options
-        if let Some(opts) = &self.options {
-            options.set_keep_alive(Duration::new(opts.keepalive_interval, 0));
+        if let Some(opts) = mqtt_options {
+            options.set_keep_alive(Duration::new(opts.keepalive_interval.unwrap_or(60), 0));
             options.set_topic_alias_max(opts.topic_alias_max);
 
             if let (Some(u), Some(p)) = (&opts.username, &opts.password) {
@@ -115,11 +134,12 @@ impl MqttBroker {
 
 impl Config {
     pub fn broker_mqtt_options(&self) -> Vec<rumqttc::v5::MqttOptions> {
+        let base_options = &self.mqtt.options;
         return self
             .mqtt
             .brokers
             .iter()
-            .map(|s| s.to_mqtt_options())
+            .map(|s| s.to_mqtt_options(base_options))
             .collect::<Vec<_>>();
     }
 }
